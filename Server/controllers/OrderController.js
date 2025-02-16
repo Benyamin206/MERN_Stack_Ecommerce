a0144481a032d3bb94b78668f9172ed0293f6d87
Benyamin206 <benyaminsibarani2406@gmail.com>
2025-02-16 21:12:22 +0700

    Create order create controller

--- Server/controllers/OrderController.js
+++ Server/controllers/OrderController.js
@@ -1,11 +1,57 @@
 import asyncHandler from "../middleware/asyncHandler.js";
+import Order from "../models/orderModel.js";
+import Product from "../models/productModel.js";
 
 
 export const CreateOrder = asyncHandler(async(req,res) => {
 
+    const {email, firstName, lastName, phone, cartItem} = req.body
+
+    if(!cartItem || cartItem.length < 0){
+        res.status(400)
+        throw new Error("Cart masih kosong")
+    }
+
+
+    let orderItem = []
+    let total = 0
+
+    for (const cart of cartItem){
+        const productData = await Product.findOne({_id : cart.product})
+
+        if(!productData){
+            res.status(404)
+            throw new Error("Id product tidak ditemukan")
+        }
+
+        const {name, price, _id} = productData
+
+        const singleProduct = {
+            name,
+            price,
+            quantity : cart.quantity,
+            product : _id
+        }
+
+        orderItem = [...orderItem, singleProduct]
+        total += cart.quantity * price
+
+    }
+
+    const order = await Order.create({
+        total,
+        itemDetail : orderItem,
+        firstName,
+        lastName,
+        email,
+        phone,
+        user : req.user.id
+    })
 
     return res.status(201).json({
         message : "Berhasil tambah order",
+        orderItem,
+        total
     })
 })
 
--- Server/models/orderModel.js
+++ Server/models/orderModel.js
@@ -47,6 +47,6 @@ const orderSchema = new Schema({
     }
 })
 
-const Order = mongoose.model("Product", orderSchemaSchema)
+const Order = mongoose.model("Order", orderSchema)
 
 export default Order
